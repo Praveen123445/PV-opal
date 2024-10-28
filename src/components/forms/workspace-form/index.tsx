70f4e2bbc56dd77ae04bdbf9c0e0f454bf9caf19
praveen verma <Pravinvarma324@gmail.com>
2024-10-28 08:23:53 +0530

    first commit

---
+++ src/components/forms/workspace-form/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const WorkspaceForm = (props: Props) => {
+  useCreateWorkspace()
+  return (
+    <div>
+      WorkspaceForm
+    </div>
+  )
+}
+
+export default WorkspaceForm
